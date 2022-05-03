c466bbacf31ef5936c5fd6407f915bad0e4af5b4
Felix Damrau <mail@felixdamrau.de>
2022-05-03 21:27:56 +0200

    Clean up empty array usage

--- src/Core/Service/WorkItemService.cs
+++ src/Core/Service/WorkItemService.cs
@@ -28,12 +28,13 @@ public class WorkItemService : IWorkItemService, IDisposable
             return Result.Fail<IReadOnlyList<Model.WorkItem>>("Service is not initialized");
 
         if (!ids.Any())
-            return Result.Ok<IReadOnlyList<Model.WorkItem>>(new List<Model.WorkItem>());
+            return Result.Ok<IReadOnlyList<Model.WorkItem>>(Array.Empty<Model.WorkItem>());
 
         var queryResults = await RunQueryAsync(workItemTrackingHttpClient, ids);
-        var workItems = new List<Model.WorkItem>();
         if (!queryResults.Valid)
             return Result.Fail<IReadOnlyList<Model.WorkItem>>(queryResults.Message);
+
+        var workItems = new List<Model.WorkItem>();
         foreach (var azureDevopsWorkItem in queryResults.Value)
         {
             var workItem = await Create(azureDevopsWorkItem, azureDevopsOrgUri, includePullRequests);
