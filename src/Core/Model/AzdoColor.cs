13eff1ffcc37c7a6be818bd1416b2d4f4ddb3b0f
Felix Damrau <mail@felixdamrau.de>
2022-10-12 18:40:02 +0200

    Add complex AzdoColor class
    * Allows to handle color string of the azdo API with 8 characters

---
+++ src/Core/Model/AzdoColor.cs
@@ -0,0 +1,23 @@
+﻿namespace Develix.AzureDevOps.Connector.Model;
+
+public class AzdoColor
+{
+    public AzdoColor(string colorString)
+    {
+        ColorString = colorString;
+        Rgb = GetRgbString(colorString);
+    }
+
+    public string ColorString { get; }
+    public string Rgb { get; }
+
+    private static string GetRgbString(string colorString)
+    {
+        return colorString switch
+        {
+            { Length: 6 } => colorString,
+            { Length: 8 } => colorString[0..6],
+            _ => "FF0000",
+        };
+    }
+}
