b71e3f27cbc6f485c9dae3d7b92f579bc62a6f66
Felix Damrau <mail@felixdamrau.de>
2022-10-13 21:48:40 +0200

    Add source/target info to demo app

--- src/App/Store/PullRequestsPageUseCase/Effects.cs
+++ src/App/Store/PullRequestsPageUseCase/Effects.cs
@@ -1,22 +1,30 @@
 ﻿using Develix.AzureDevOps.Connector.Service;
 using Fluxor;
+using MudBlazor;
 
 namespace Develix.AzureDevOps.Connector.App.Store.PullRequestsPageUseCase;
 
 public class Effects
 {
     private readonly IReposService reposService;
+    private readonly ISnackbar snackbar;
 
-    public Effects(IReposService reposService)
+    public Effects(IReposService reposService, ISnackbar snackbar)
     {
         this.reposService = reposService;
+        this.snackbar = snackbar;
     }
 
     [EffectMethod]
     public async Task HandleGetPullRequestsAction(GetPullRequestsAction action, IDispatcher dispatcher)
     {
         var pullRequests = reposService.GetPullRequests(action.Ids);
-        var resultAction = new GetPullRequestsResultAction(await pullRequests.ToListAsync().ConfigureAwait(false));
+        var results = await pullRequests.ToListAsync().ConfigureAwait(false);
+        var resultAction = new GetPullRequestsResultAction(results.Where(r => r.Valid).Select(r => r.Value).ToList());
+        foreach (var message in results.Where(r => !r.Valid).Select(r => r.Message))
+        {
+            snackbar.Add(message, Severity.Warning);
+        }
         dispatcher.Dispatch(resultAction);
     }
 }
